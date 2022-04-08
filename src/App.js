22db5d7cbb6dd6c9ebd9ec3c627d644e93f14840
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-08 12:54:01 +0200

    installed uuid to generate a random id  and created the data of the song

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 //import styles
 import './styles/app.scss'
+//import util
+import data from './util';
 
 function App() {
   return (
