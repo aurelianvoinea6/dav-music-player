d4a4818ddaca60b2968bd92354f5b1a22334d988
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-08 17:34:44 +0200

    styled the library component

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,11 @@ const LibrarySong = ({ song }) => {
     return(
        <div className="library-song">
            <img src={song.cover} alt="cover"></img>
+           <div className="song-description">
+
            <h3>{song.name}</h3>
            <h4>{song.artist}</h4>
+           </div>
        </div>
     );
 };
