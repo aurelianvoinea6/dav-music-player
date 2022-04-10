ecd84a6b541205749ae31f9535702b55b7ad992b
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-04-10 05:38:27 +0200

    fixed the bug for skiping to the next song

--- src/App.js
+++ src/App.js
@@ -42,7 +42,14 @@ function App() {
     let currentIndex = songs.findIndex((song) =>song.id === currentSong.id);
     await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
     if(isPlaying) audioRef.current.play();
-  };
+  }
+  if (isPlaying) {
+    const playPromise = audioRef.current.play();
+
+    if (playPromise !== undefined) {
+      playPromise.then((_) => {}).catch((error) => {});
+    }
+  }
   return (
     <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
